1d7872095a5adf4d668490a7e28092099374a6ad
Kid <kidonng@gmail.com>
2020-03-26 21:24:53 +0800

    Vanilla is the best

---
+++ src/script.ts
@@ -0,0 +1,33 @@
+import ga from 'ga-lite'
+
+ga('create', 'UA-140053908-1', 'auto')
+ga('send', 'pageview')
+
+const bio = document.querySelector<HTMLElement>('.bio')
+const avatar = document.querySelector<HTMLElement>('.avatar')
+const close = document.querySelector<HTMLElement>('.close')
+let mouse = true
+
+const toggleBio = (e: { x: number; y: number }) => {
+  bio.classList.toggle('hidden')
+  // Not mouse triggered click
+  if (e.x === 0 && e.y === 0) {
+    if (bio.classList.contains('hidden')) avatar.focus()
+    else mouse = false
+  }
+}
+
+const adjustFocus = () => {
+  if (!bio.classList.contains('hidden') && !mouse) {
+    mouse = true
+    close.focus()
+  }
+}
+
+bio.addEventListener('transitionstart', adjustFocus)
+avatar.addEventListener('click', toggleBio)
+close.addEventListener('click', toggleBio)
+document.addEventListener('keydown', (e) => {
+  if (!bio.classList.contains('hidden') && e.key === 'Escape')
+    toggleBio({ x: 0, y: 0 })
+})
