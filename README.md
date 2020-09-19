0375af9705fe76b041ed34061c9dc221020a41ee
Kid <kidonng@gmail.com>
2020-09-19 14:14:46 +0800

    Remove about section

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # [`xuann.wang`](https://xuann.wang/)
 
-This is [@kidonng](https://github.com/kidonng)'s homepage, a single page optimized for **speed** (inline assets & CDN), **content** (fine-tuned `<head>` & language switch) and **accessibility** (keyboard-friendly & adaptive color scheme).
+This is [@kidonng](https://github.com/kidonng)'s homepage, a roughly crafted single page.
 
-You are welcome to use the layout to make your own homepage. ~~though it's not customizable enough~~.
-
-**Notes**: the tags are for CDN, the website doesn't have a fixed version.
+**Notes**: The tags are for CDN. The website doesn't have a fixed release.
 
 ## I'm Feeling Lucky
 
-`dig hi.xuann.wang TXT +short` or `nslookup -type=TXT hi.xuann.wang`.
+Try `dig hi.xuann.wang TXT +short` or `nslookup -type=TXT hi.xuann.wang`.
--- src/script.ts
+++ src/script.ts
@@ -1,48 +1,6 @@
 import ga from 'ga-lite'
+import 'minireset.css'
+import './style.css'
 
 ga('create', 'UA-140053908-1', 'auto')
 ga('send', 'pageview')
-
-const about = document.querySelector<HTMLElement>('.about')
-const avatar = document.querySelector<HTMLElement>('.avatar')
-const language = document.querySelector<HTMLElement>('.language')
-const close = document.querySelector<HTMLElement>('.close')
-const english = document.querySelector<HTMLElement>('.english')
-const chinese = document.querySelector<HTMLElement>('.chinese')
-const profile = document.querySelector<HTMLElement>('.profile')
-const email = document.querySelectorAll<HTMLAnchorElement>('.email')
-let mouse = true
-
-const toggleAbout = (e: { x: number; y: number }) => {
-  about.classList.toggle('hidden')
-  profile.classList.toggle('hidden')
-
-  // Not mouse triggered toggle
-  if (e.x === 0 && e.y === 0) {
-    if (about.classList.contains('hidden')) avatar.focus()
-    else mouse = false
-  }
-}
-
-const adjustFocus = () => {
-  if (!about.classList.contains('hidden') && !mouse) {
-    mouse = true
-    close.focus()
-  }
-}
-
-const switchLanguage = () => {
-  english.hidden = !english.hidden
-  chinese.hidden = !chinese.hidden
-}
-
-about.addEventListener('transitionstart', adjustFocus)
-avatar.addEventListener('click', toggleAbout)
-language.addEventListener('click', switchLanguage)
-close.addEventListener('click', toggleAbout)
-document.addEventListener('keydown', (e) => {
-  if (!about.classList.contains('hidden') && e.key === 'Escape')
-    toggleAbout({ x: 0, y: 0 })
-})
-
-email.forEach((el) => (el.href = `mailto:${atob('aGlAeHVhbm4ud2FuZw==')}`))
