c051db24a5f62cc10055af4d3762c3fe9a2bd0c0
Kid <kidonng@gmail.com>
2021-03-18 11:20:35 +0800

    Add <noscript> content

--- src/script.ts
+++ src/script.ts
@@ -5,3 +5,5 @@ ga('send', 'pageview')
 
 const email = document.getElementById('email') as HTMLAnchorElement
 email.href = `mailto:${atob('aGlAeHVhbm4ud2FuZw==')}`
+
+console.info("Some people put cool stuff in the console, but I won't.")
